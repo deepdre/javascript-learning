e71528697274e48d5243f79e58afb7e0ece80128
deepdre <79251253+deepdre@users.noreply.github.com>
2025-04-01 13:05:34 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict";  //treat all js code as newer version
+//alert(3+3); we are usin nodejs , not browser
+
+let name = "deep"
+let age = 6
+let isLoggedIn = false 
+
+// number = 2 to power 53
+// bigint 
+//string
+// boolean
+// null => standalone value
+// undefined = 
+// symbol
+
+
+// object 
+console.log(typeof "hitesh");
+console.log(typeof null); // object 
+
