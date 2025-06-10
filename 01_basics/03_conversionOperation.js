21b2dc2e274a1f8d91f5257b617c253327a097be
deepdre <79251253+deepdre@users.noreply.github.com>
2025-06-10 00:19:18 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,21 @@
+let score = "hello45"
+console.log(typeof score);
+console.log(typeof(score))
+
+let valueInNumber = Number(score);
+console.log(valueInNumber);
+console.log(typeof(valueInNumber));
+
+//"33" = 33
+// "33abc" = nan
+//true = 1 false =0 
+let isLoggedIn = 0
+let booleanValue = Boolean(isLoggedIn);
+console.log(booleanValue);
+
+// 1 = treu 0 = false , "" = false , "deep" = true
+
+let someNumber = 33
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
